fb15de572577d56181f029b07820797206d1a22d
DuongKien <ddk0705@gmail.com>
2019-03-17 17:34:36 +0700

    Add firebase SDK

--- android/app/src/main/java/com/trust/MainApplication.java
+++ android/app/src/main/java/com/trust/MainApplication.java
@@ -3,8 +3,8 @@ package com.trust;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.syan.agora.AgoraPackage;
@@ -29,8 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNI18nPackage(),
             new RNGoogleSigninPackage(),
+            new RNI18nPackage(),
             new FBSDKPackage(),
             new ReactNativeConfigPackage(),
             new AgoraPackage(),
