9c021f5040cd2cc8f23b067b221cda54ae449029
DuongKien <ddk0705@gmail.com>
2019-03-24 01:58:41 +0700

    Refactor code

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FlashMessage from 'react-native-flash-message';
+import { Router, Scene, Drawer } from 'react-native-router-flux';
 
 //Redux
 import { createStore, applyMiddleware } from 'redux';
@@ -8,25 +9,64 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './src/reducers';
 import rootSaga from './src/sagas/rootSaga';
 
+// Screens
+import HomeScreen from './src/containers/Home';
+import LoginScreen from './src/containers/Login';
+
 // Components
 import Loading from './src/components/Loading';
 import Statusbar from './src/components/Statusbar';
 import AlertDialog from './src/components/AlertDialog';
-import RouterComponent from './src/components/Router';
+import SideMenu from './src/components/SideMenu';
+import MenuButton from './src/components/MenuButton';
 
 // Config
 import config from './src/assets/data/config.json';
+
+// Styles
+import { mainStyles } from './src/styles';
+
+// Services
+import { APIService } from './src/services/api';
+const apiService = new APIService();
 
 // Create store
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
-export default class App extends Component<{}> {
+interface IProps {
+
+}
+
+interface IState {
+    logged: boolean;
+}
+
+export default class App extends Component<IProps, IState> {
+
+    state = {
+        logged: false,
+    }
 
     constructor(props: any) {
         super(props);
     }
+
+    async componentDidMount() {
+        try {
+            await apiService.isLogged();
+            this.setState({ logged: true });
+        } catch (e) {
+            this.setState({ logged: false });
+        }
+    }
+
+    renderMenuButton() {
+        return (
+            <MenuButton/>
+        );
+    };
 
     render() {
         console.disableYellowBox = true;
@@ -36,7 +76,34 @@ export default class App extends Component<{}> {
 
                 <Statusbar />
 
-                <RouterComponent />
+                <Router>
+                    <Drawer
+                        contentComponent={SideMenu}
+                        drawer
+                        initial
+                        drawerPosition={mainStyles.drawer.position}
+                    >
+                        <Scene key="root">
+                            <Scene
+                                key="login"
+                                component={LoginScreen}
+                                initial={!this.state.logged}
+                                hideNavBar={true}
+                            />
+
+                            <Scene
+                                navigationBarStyle={mainStyles.navigationBar}
+                                key="home"
+                                component={HomeScreen}
+                                title="Home"
+                                initial={this.state.logged}
+                                hideNavBar={false}
+                                renderLeftButton={this.renderMenuButton}
+                                titleStyle={mainStyles.titleHeader}
+                            />
+                        </Scene>
+                    </Drawer>
+                </Router>
 
                 <FlashMessage
                     position={config['alert']['position']}
