d425eb71214aa8be5221481992f1a1ec2f08e44b
DuongKien <ddk0705@gmail.com>
2019-04-03 23:47:52 +0700

    Fix issue Agora

--- src/containers/AddFriends/AddFriends.tsx
+++ src/containers/AddFriends/AddFriends.tsx
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { Container, Content } from 'native-base';
+
+// Styles
+import { styleSheet, styles } from './styles';
+import { mainStyles } from '../../styles';
 
 interface IProps {
 
 }
 
 interface IState {
-
+    searchValue: string;
 }
 
 export class AddFriendsScreen extends Component<IProps, IState> {
+
+    state = {
+        searchValue: '',
+    }
 
     constructor(props: any) {
         super(props);
@@ -19,11 +29,33 @@ export class AddFriendsScreen extends Component<IProps, IState> {
 
     }
 
+    onChangeSearch(val: any) {
+        this.setState({
+            searchValue: val
+        }, () => {
+            console.log(this.state.searchValue);
+        });
+    }
+
+    onClearText() {
+        console.log('onClearText');
+    }
+
     render() {
         return (
-            <View>
-                <Text>Add Friends Screen</Text>
-            </View>
+            <Container>
+                <Content>
+                    <View style={styleSheet.searchContent}>
+                        <SearchBar
+                            placeholder="Type Here..."
+                            onChangeText={(val) => this.onChangeSearch(val)}
+                            value={this.state.searchValue}
+                            clearIcon={styles.clearIcon}
+                            onClearText={() => this.onClearText()}
+                        />
+                    </View>
+                </Content>
+            </Container>
         );
     }
 
---
+++ src/containers/AddFriends/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+import { colors, mainFont, dimensions } from '../../styles';
+
+export const styleSheet = StyleSheet.create({
+    searchContent: {
+
+    },
+});
+
+export const styles = {
+    clearIcon: {
+        color: '#86939e',
+        name: 'clear',
+    },
+}
